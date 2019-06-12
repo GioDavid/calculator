d52a5a8a7dacea91d7b6bc13be780ac103650b55
David <david@Davids-MacBook-Pro-4.local>
2019-06-12 06:50:37 -0500

    reformat code

--- src/operations/operations.test.js
+++ src/operations/operations.test.js
@@ -1,27 +1,26 @@
 import { divide, multiply, sum, substract } from './operations';
 
 it('sums numbers', () => {
-    expect(sum(1, 2)).toEqual(3);
-    expect(sum(2, 2)).toEqual(4);
-    expect(sum(2, 0)).toEqual(2);
-  });
+  expect(sum(1, 2)).toEqual(3);
+  expect(sum(2, 2)).toEqual(4);
+  expect(sum(2, 0)).toEqual(2);
+});
 
-  it('substract numbers', () => {
-    expect(substract(1, 2)).toEqual(-1);
-    expect(substract(2, 2)).toEqual(0);
-    expect(substract(2, 0)).toEqual(2);
-  });
+it('substract numbers', () => {
+  expect(substract(1, 2)).toEqual(-1);
+  expect(substract(2, 2)).toEqual(0);
+  expect(substract(2, 0)).toEqual(2);
+});
 
-  it('multiply numbers', () => {
-    expect(multiply(1, 2)).toEqual(2);
-    expect(multiply(2, 4)).toEqual(8);
-    expect(multiply(2, 0)).toEqual(0);
-  });
+it('multiply numbers', () => {
+  expect(multiply(1, 2)).toEqual(2);
+  expect(multiply(2, 4)).toEqual(8);
+  expect(multiply(2, 0)).toEqual(0);
+});
 
-
-  it('divide numbers', () => {
-    expect(divide(1, 2)).toEqual(0.5);
-    expect(divide(4, 2)).toEqual(2);
-    expect(divide(2, 0)).toEqual(Infinity);
-    expect(divide(0, 2)).toEqual(0);
-  });
+it('divide numbers', () => {
+  expect(divide(1, 2)).toEqual(0.5);
+  expect(divide(4, 2)).toEqual(2);
+  expect(divide(2, 0)).toEqual(Infinity);
+  expect(divide(0, 2)).toEqual(0);
+});
